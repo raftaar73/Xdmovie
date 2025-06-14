a8560d0802ca00ec4ce62ca2be524107b3364154
S3HID N3W3B <166154843+NBOTZ001@users.noreply.github.com>
2025-06-14 11:37:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://fz0wt:zcFkRMhNdZh9Fs5x@cluster0.gwrit4f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "NBX")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'nawab_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY',False))
